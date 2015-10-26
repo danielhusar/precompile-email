ef23a611014d4be3a3c3bf1041a0e7f1cffb6cff
Daniel Husar <dano.husar@gmail.com>
2015-10-26 13:20:12 +0000

    init

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+'use strict';
+
+// Include gulp
+var gulp = require('gulp');
+var express = require('express');
+var browser = require('open');
+var clipboard = require("gulp-clipboard");
+var premailer = require('gulp-premailer');
+
+// Tasks aliases
+gulp.task('default', function () {
+	var mq;
+	gulp.src('html/latest.html')
+		.pipe(premailer())
+		.pipe(clipboard())
+		.pipe(gulp.dest('output'));
+});
+
+// Watch Files For Changes
+gulp.task('watch', function () {
+  var app = express();
+  app.use(express.static('./html'));
+  app.use(directory('./html'));
+  app.listen(8090);
+});
