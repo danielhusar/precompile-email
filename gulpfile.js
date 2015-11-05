00ae6772c9e2ea6e77d08622421e704c07998d35
Daniel Husar <dano.husar@gmail.com>
2015-11-05 17:38:51 +0000

    Cleanup

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,17 @@
 'use strict';
 
-// Include gulp
 var gulp = require('gulp');
 var express = require('express');
 var clipboard = require("gulp-clipboard");
 var premailer = require('gulp-premailer');
 
-// Tasks aliases
 gulp.task('default', function () {
-	var mq;
-	gulp.src('html/latest.html')
-		.pipe(premailer())
-		.pipe(clipboard())
-		.pipe(gulp.dest('output'));
+  gulp.src('html/latest.html')
+    .pipe(premailer())
+    .pipe(clipboard())
+    .pipe(gulp.dest('output'));
 });
 
-// Watch Files For Changes
 gulp.task('watch', function () {
   var app = express();
   app.use(express.static('./html'));
